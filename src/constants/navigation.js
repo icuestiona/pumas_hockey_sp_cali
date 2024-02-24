cc93e03664aa10469cfe9c004b50547f8447da03
Soraya <icuestiona@gmail.com>
2024-02-25 00:18:39 +0100

    Created constants folder & navigation

---
+++ src/constants/navigation.js
@@ -0,0 +1,9 @@
+export const navigation = [
+  { name: "Home", path: "/" },
+  { name: "Who We Are", path: "/who-we-are" },
+  { name: "Team", path: "/team" },
+  { name: "Testimonials", path: "/testimonials" },
+  { name: "Join Us", path: "/join-us" },
+  { name: "Documents", path: "/documents" },
+  { name: "Contact", path: "/contact" },
+];
