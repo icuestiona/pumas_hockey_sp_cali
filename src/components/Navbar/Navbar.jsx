2514a8e8a50307b1b3a69793c1b09cc24b134a39
Soraya <icuestiona@gmail.com>
2024-02-25 00:17:33 +0100

    Navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { navigation } from "../constants/navigation";
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        {navigation.map((item, index) => (
+          <li key={index} className="nav-item">
+            <a href={item.path} className="nav-link">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
