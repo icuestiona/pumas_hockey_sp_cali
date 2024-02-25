fd00564b7546c87febbefad231b2214d52314d3c
Soraya <icuestiona@gmail.com>
2024-02-25 20:30:32 +0100

    Hero component added in Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Hero from "../components/Hero/Hero";
 
-function Homepage() {
-  return <div>Homepage</div>;
-}
-
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Hero />
+    </div>
+  );
+};
 export default Homepage;
