3b0501eb9c255ac1816cbf048d1f556023809e2e
Soraya <icuestiona@gmail.com>
2024-02-25 11:04:55 +0100

    Navbar imported again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Homepage from "./pages/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
