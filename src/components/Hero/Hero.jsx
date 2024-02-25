d2c7da476e3010ec3c8bf2791a7722937ee55dc2
Soraya <icuestiona@gmail.com>
2024-02-26 00:16:12 +0100

    Images changed and modified styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Carousel, CarouselItem, CarouselCaption } from "react-bootstrap";
 import "./Hero.css";
-import image25 from "../Assets/image25.jpg";
+import image26 from "../Assets/image26.jpg";
 import image17 from "../Assets/image17.jpg";
-import image32 from "../Assets/image32.jpg";
+import image34 from "../Assets/image34.jpg";
+import image10 from "../Assets/image10.jpg";
 import image37 from "../Assets/image37.jpg";
 import image40 from "../Assets/image40.jpg";
+import image8 from "../Assets/image8.jpg";
 
 const Hero = () => {
   const images = [
     {
-      src: image25,
+      src: image40,
+      alt: "Fifth slide",
+    },
+    {
+      src: image26,
       alt: "First slide",
     },
     {
@@ -18,16 +24,20 @@ const Hero = () => {
       alt: "Second slide",
     },
     {
-      src: image32,
+      src: image34,
       alt: "Third slide",
+    },
+    {
+      src: image10,
+      alt: "Tenth slide",
     },
     {
       src: image37,
       alt: "Fourth slide",
     },
     {
-      src: image40,
-      alt: "Fifth slide",
+      src: image8,
+      alt: "Sixth slide",
     },
   ];
 
@@ -53,13 +63,15 @@ const Hero = () => {
           <div className="hero-image-overlay">
             <img className="d-block w-100" src={image.src} alt={image.alt} />
             <div className="hero-image-overlay-content">
-              <h1>ENJOYMENT, LEARNING, AND SPORTS TRAINING FOR A LIFETIME!</h1>
+              <h1>
+                Enjoyment, learning and <br />
+                lifelong sports training!
+              </h1>
               <p>
-                Our club offers to each of our children and their families,
-                enjoyable sporting experiences, full of learning, that
-                contribute to their integral formation.
+                Offering our children and their families fun sports experiences,
+                full of lessons, which contribute to their integral development.
               </p>
-              <button type="button" className="btn btn-light">
+              <button type="button" className="btn btn-info">
                 Ask for a free class
               </button>
             </div>
