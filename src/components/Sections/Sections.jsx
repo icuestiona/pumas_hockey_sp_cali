b42d630ab4e4f0e31ebc8173227731367b8d5e98
Soraya <icuestiona@gmail.com>
2024-02-26 23:34:17 +0100

    Component Sections created and styled

---
+++ src/components/Sections/Sections.jsx
@@ -0,0 +1,124 @@
+// SectionsComponent.jsx
+
+import React from "react";
+import "./Sections.css";
+import imageValuesWorkshops from "../Assets/image1.jpg";
+import imageSportsTraining from "../Assets/image11.jpg";
+import imageRollerHockey from "../Assets/image47.jpg";
+import imageSocialInclusion from "../Assets/image43.jpg";
+
+const SectionsComponent = () => {
+  return (
+    <div className="sections-container">
+      {/* Section 1 */}
+      <section className="section">
+        <div className="content">
+          <div className="text">
+            <h2>Unveil the thrills of Roller Hockey</h2>
+            <p>
+              Welcome to a world where roller hockey isn't just a game; it's a
+              lifestyle! Picture this: two teams, five players each, racing on
+              parallel four-wheeled skates, weaving through a track enclosed by
+              rounded corners or shaped like a pickaxe. With a goal in sight,
+              they skillfully wield curved wooden sticks, propelling a rubber
+              ball with compressed cork – that's the electrifying essence of
+              roller hockey! Embrace The PUMAS way: where hockey meets
+              lifestyle! <span>🏒🚀</span>
+            </p>
+            <p>
+              At PUMAS Hockey Club, we don't just play hockey; we live it.
+              Here's a sneak peek into the PUMAS Way:
+            </p>
+          </div>
+          <div className="image">
+            <img src={imageRollerHockey} alt="Roller Hockey" />
+          </div>
+        </div>
+      </section>
+
+      {/* Section 2 */}
+      <section className="section">
+        <div className="content two-columns">
+          <div className="image-column">
+            <div className="image">
+              <img src={imageSportsTraining} alt="Sports Training" />
+            </div>
+          </div>
+          <div className="text-column">
+            <h2>Sports Training and Education</h2>
+            <p>
+              Step into our dynamic hockey training sessions – a blend of
+              technical prowess, physical agility, and tactical brilliance. Our
+              approach is fun, creative, and designed to unleash the hockey star
+              within you!
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Section 3 */}
+      <section className="section">
+        <div className="content">
+          <div className="text">
+            <h2>Values Workshops</h2>
+            <p>
+              Beyond the rink, we delve into leisure and extra sports activities
+              that go beyond goals and scores. Our workshops focus on building
+              values and teamwork, ensuring every member grows not only as a
+              player but as an individual.
+            </p>
+          </div>
+          <div className="image">
+            <img src={imageValuesWorkshops} alt="Values Workshops" />
+          </div>
+        </div>
+      </section>
+
+      {/* Section 4 */}
+      <section className="section">
+        <div className="content two-columns">
+          <div className="image-column">
+            <div className="image">
+              <img
+                src={imageSocialInclusion}
+                alt="Social and Inclusion Activities"
+              />
+            </div>
+          </div>
+          <div className="text-column">
+            <div className="text">
+              <h2>Social and Inclusion Activities</h2>
+              <p>
+                It's not just about the game; it's about the community. Join us
+                for socialization and inclusion activities - birthdays, warm
+                welcomes, educational outings, farewells, and special occasions.
+                At PUMAS Hockey Club, everyone is part of something
+                extraordinary.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Closing Statement */}
+      <section className="closing-statement">
+        <div className="text">
+          <p>
+            The PUMAS way – Where great hockey players meet great people! Every
+            day, we evaluate the performance of our young athletes. As they
+            evolve, we tailor our efforts to strengthen their skills.
+            Recognizing the standouts, we inspire them to aim for the stars and
+            become high-performance athletes. We're not just shaping incredible
+            hockey players; we're molding remarkable individuals.
+          </p>
+          <p>
+            Ready to embrace the thrill of roller hockey? Join us at our club!
+            <span>🏒🌟</span>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SectionsComponent;
