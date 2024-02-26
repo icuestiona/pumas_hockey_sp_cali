ae071b1040de9bf0656c8bb308782d3cf0402aaf
Soraya <icuestiona@gmail.com>
2024-02-26 23:34:34 +0100

    Added Sections

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Sections from "../components/Sections/Sections";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Hero />
+      <Sections />
     </div>
   );
 };
