58cdd195af5cac998e3c5aae424cda6460e6911a
Soraya <icuestiona@gmail.com>
2024-02-28 01:10:50 +0100

    Requirements component added in Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import Sections from "../components/Sections/Sections";
+import Requirements from "../components/Requirements/Requirements";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Hero />
       <Sections />
+      <Requirements />
     </div>
   );
 };
