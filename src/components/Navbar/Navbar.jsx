d7f8f85ca607340adbf34fc9aba5d4e21bf59ead
Soraya <icuestiona@gmail.com>
2024-03-02 18:01:32 +0100

    Added isNavOpen & toggleNav to control the visibility of the navigation menu on small devices.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { navigation } from "../../constants/navigation";
 import clubLogo from "../Assets/club-logo.jpg";
@@ -13,67 +13,91 @@ import {
 import { faPhone, faEnvelope } from "@fortawesome/fontawesome-free-solid";
 
 const Navbar = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+
   const styles = {
     icon: "icon",
   };
 
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
+
   return (
-    <nav className="navbar d-flex justify-content-between fixed-top navbar-expand-lg navbar-light bg-light p-2">
-      <div className="left-section">
-        <div className="logo">
-          <img src={clubLogo} alt="Club Logo" />
+    <nav
+      className={`navbar navbar-expand-lg navbar-light bg-light p-2 ${
+        isNavOpen ? "open" : ""
+      }`}
+    >
+      <div className="container">
+        <div className="left-section">
+          <div className="logo">
+            <img src={clubLogo} alt="Club Logo" />
+          </div>
+
+          <button className="navbar-toggler" type="button" onClick={toggleNav}>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <ul className={`navbar-nav ${isNavOpen ? "open" : ""}`}>
+            {navigation.map((item, index) => (
+              <li key={index} className="nav-item">
+                <a
+                  href={item.path}
+                  className="nav-link"
+                  onClick={() => setNavOpen(false)}
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
 
-        <ul className="navbar-nav">
-          {navigation.map((item, index) => (
-            <li key={index} className="nav-item">
-              <a href={item.path} className="nav-link">
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="right-section">
-        <div className="social-links">
-          <a
-            href="https://es-es.facebook.com/pumashockeysp/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
-          </a>
-          <a
-            href="https://www.instagram.com/pumascalihockeyclubsp/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCZIn_-lL7yx4y1GRQ-UGpEQ"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
-          </a>
-          <a
-            href="https://twitter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faSquareXTwitter} className={styles.icon} />
-          </a>
-          <a href="tel:+1234567890">
-            <FontAwesomeIcon icon={faPhone} className={styles.icon} />
-          </a>
-          <a
-            href="Pumascalihockeyclubsp@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
-          </a>
+        <div className="right-section">
+          <div className="social-links">
+            <a
+              href="https://es-es.facebook.com/pumashockeysp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+            </a>
+            <a
+              href="https://www.instagram.com/pumascalihockeyclubsp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCZIn_-lL7yx4y1GRQ-UGpEQ"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
+            </a>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faSquareXTwitter}
+                className={styles.icon}
+              />
+            </a>
+            <a href="tel:+1234567890">
+              <FontAwesomeIcon icon={faPhone} className={styles.icon} />
+            </a>
+            <a
+              href="Pumascalihockeyclubsp@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+            </a>
+          </div>
         </div>
       </div>
     </nav>
