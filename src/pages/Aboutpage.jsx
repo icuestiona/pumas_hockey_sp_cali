5a04e5d98606fcc9369b9872ff5f9327ed12abe7
Soraya Carvajal <icuestiona@gmail.com>
2024-07-28 00:54:21 +0200

    Added about structure and images

---
+++ src/pages/Aboutpage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import "./Aboutpage.css";
+import image61 from "../components/Assets/Pumas2.jpg";
+import image62 from "../components/Assets/Pumas5.jpg";
+import image63 from "../components/Assets/Pumas4.jpg";
+import image64 from "../components/Assets/Pumas3.jpg";
+import image65 from "../components/Assets/Pumas12.jpg";
+
+
+const AboutPage = () => {
+  return (
+    <div className="about-page">
+    <div className="about-container">
+    <h1>Pumas Club, boosting the growth of roller hockey in our region </h1>
+    <div className="about-content">
+      <div className="about-column">
+           <img src={image64} alt="Pumas team" />
+      <p>
+      The Club Pumas Hockey SP in Cali, Colombia is a team that represents the rich history and revival of roller hockey in Valle del Cauca, Colombia. Roller hockey in our region has a history that dates back 50 years, being the first skating discipline that led to the creation of the Valle del Cauca Skating League, originally known as the Hockey and Skating League of Valle del Cauca. <br/> <br/>
+      It is important to mention that The Valle del Cauca Roller Hockey Team has achieved a total of 23 national titles, making them the team with the most championships in this discipline to date, with their last victory recorded in 2009. They have also celebrated three championships in the National Games in 1988, 1992, and 1996, and were runners-up in 2004 when roller hockey was reintroduced to this sporting event. <br/> <br/>
+      In Valle del Cauca, until the 2000s, there were twelve roller hockey clubs, with Cali being home to six of them: América, Mequin, José Noel Franco, Independiente, Siete Ríos, and Micromalla. Additionally, roller hockey was practiced in various educational institutions such as Berchmans, San Luis Gonzaga, and Reyes Católico schools.  <br/> <br/>
+      Another club was founded in Buga in 1993, which later transformed into Los Huracanes in 1998, while the Club Bengala de Tuluá emerged in 1999. In 2010, with the support of the Cali team coach, Club Barcelona was established in Yumbo, further diversifying the roller hockey offerings in the region. <br/> <br/>
+      </p>
+      <img src={image63} alt="Pumas player" />
+      <p>
+        <h4>
+        Hard times and the decline of Roller Hockey in our region
+        </h4>
+      Despite the achievements in roller hockey, Cali witnessed the progressive disappearance of this discipline in 2011. This phenomenon is attributed to the lack of interest and continuity from former players, as well as the limited attention and support provided by regional sports institutions. Consequently, the clubs in Tuluá and Yumbo also ceased their activities in 2018 and 2019, respectively, highlighting a crisis in the development of roller hockey in our department.
+      </p>
+      <p>
+      However, in 2016, two former players and a group of parents took the initiative to revive roller hockey in Cali by creating the Pumas Cali Hockey Club SP, the only roller hockey club in the city. Due to the allocation of the original venue to other sports and the consequent lack of facilities for practice, the club requested and obtained establishment in the Vallecaucana de Patinaje League, specifically at the Hockey Patín Luz Mery Tristán – Unidad Jaime Soto Aparicio rink. <br/> <br/>
+      Initially, the club operated with a restricted schedule of three days a week and four and a half hours of weekly practice. However, the programming was later expanded to the whole week, resulting in a total of eight hours of weekly activity.
+      </p>
+          <img src={image62} alt="Pumas children" />
+      <h4>
+      Club Pumas, the revival of the sport and achievements 
+      </h4>
+      <p>
+      Pumas Cali Hockey Club SP stands as an outstanding example of roller hockey practice, with 34 players, both male and female, ranging in age from 3 to 40 years old, from Cali, Yumbo, Jamundí, and Tuluá. <br/> <br/>
+      Among these athletes, some come from vulnerable contexts such as Siloé and Las Palmas neighbourhoods, as well as rural areas like Montebello. Representing the youth, under 19, and senior categories, the club hosts the only women's team in Valle del Cauca, dedicated to revitalizing this discipline in the city of Cali and its surroundings.<br/> <br/>
+      The Club Pumas Hockey SP has achieved several accomplishments, including being champions in the Women Under 19 category in 2024, third place twice in the Open Women's category in 2024, third place twice in the Under 19 category in 2023 and 2024, and third place nationally in skills with the Roller Hockey School. <br/> <br/>
+      Additionally, three female players were called up and participated in the Colombian Roller Hockey National Team at the Pan American Roller Hockey Championship held in Bogotá in 2024.
+      </p>
+         <img src={image61} alt="Pumas team" />
+      <h4>
+      Challenges and difficulties
+      </h4>
+      <p>
+      Despite these achievements and advancements in the revival of roller hockey in Cali and Valle del Cauca, the consolidation of this sport faces serious difficulties. The lack of interest and support from former players, as well as from sports institutions in Cali, Yumbo, and Tuluá, limits its development. <br/> <br/>
+      Moreover, restricted access to suitable sports venues for roller hockey practice, combined with inadequate conditions of the available spaces, significantly hinders progress and the formation of new talent in the discipline.<br/> <br/>
+      However, the club has made significant progress by hosting and organizing three tournaments of its own, called the Pumas Cup, in the years 2017, 2018, and 2019. Additionally, in 2021, a one-week camp was conducted under the guidance of the former coach of the Colombian Roller Hockey National Team, Portuguese André Mato, who highlighted the club's commitment to training and sports development.<br/> <br/>
+      <img src={image65} alt="Pumas team" />
+      To consolidate roller hockey and strengthen the sports training of the club's athletes, a four-week Hockey Technification Clinic was held in the city of Cali between July and August 2024. <br/> <br/>
+      This initiative was designed to improve the technical capabilities of the athletes, thereby optimizing their performance. It also aimed to train the coaches of Pumas and Valle del Cauca teams, who, as former experienced players, contribute contractually and voluntarily to the development of this discipline, enriching the comprehensive training of the participants.<br/> <br/>
+      In this regard, the international camp represented a significant step in the consolidation of the Roller Hockey team, promoting social values and excellent sports technique. The goal is to prepare the members to excel in both roller and conventional hockey, aiming to reach the required levels in national competition. This initiative directly contributes to the fulfillment of our vision, focused on the high performance of Roller Hockey.
+  </p>
+  </div>
+  </div>
+  </div>
+  </div>
+  );
+};
+
+export default AboutPage;
